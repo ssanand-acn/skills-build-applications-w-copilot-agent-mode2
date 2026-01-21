41c229d90f7d7f8c9c54908e95d2d965750f167a
ssanand-acn <anand.ss@accenture.com>
2026-01-21 16:32:17 +0000

    commit 2

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,42 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import Team, User, Activity, Workout, Leaderboard
+from django.utils import timezone
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **kwargs):
+        # Delete existing data
+        Leaderboard.objects.all().delete()
+        Activity.objects.all().delete()
+        Workout.objects.all().delete()
+        User.objects.all().delete()
+        Team.objects.all().delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        # Create users
+        tony = User.objects.create(name='Tony Stark', email='tony@marvel.com', team=marvel)
+        steve = User.objects.create(name='Steve Rogers', email='steve@marvel.com', team=marvel)
+        bruce = User.objects.create(name='Bruce Wayne', email='bruce@dc.com', team=dc)
+        clark = User.objects.create(name='Clark Kent', email='clark@dc.com', team=dc)
+
+        # Create activities
+        Activity.objects.create(user=tony, type='Running', duration=30, date=timezone.now())
+        Activity.objects.create(user=steve, type='Cycling', duration=45, date=timezone.now())
+        Activity.objects.create(user=bruce, type='Swimming', duration=60, date=timezone.now())
+        Activity.objects.create(user=clark, type='Yoga', duration=20, date=timezone.now())
+
+        # Create workouts
+        w1 = Workout.objects.create(name='Super Strength', description='Strength training for superheroes')
+        w2 = Workout.objects.create(name='Flight Training', description='Aerobic workout for flying heroes')
+        w1.suggested_for.set([tony, bruce])
+        w2.suggested_for.set([steve, clark])
+
+        # Create leaderboards
+        Leaderboard.objects.create(team=marvel, points=100)
+        Leaderboard.objects.create(team=dc, points=90)
+
+        self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data.'))
---
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -0,0 +1,29 @@
+from djongo import models
+
+class Team(models.Model):
+    _id = models.ObjectIdField(primary_key=True, editable=False)
+    name = models.CharField(max_length=100, unique=True)
+
+class User(models.Model):
+    _id = models.ObjectIdField(primary_key=True, editable=False)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='members')
+
+class Activity(models.Model):
+    _id = models.ObjectIdField(primary_key=True, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='activities')
+    type = models.CharField(max_length=100)
+    duration = models.IntegerField()  # in minutes
+    date = models.DateField()
+
+class Workout(models.Model):
+    _id = models.ObjectIdField(primary_key=True, editable=False)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    suggested_for = models.ManyToManyField(User, related_name='suggested_workouts')
+
+class Leaderboard(models.Model):
+    _id = models.ObjectIdField(primary_key=True, editable=False)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='leaderboards')
+    points = models.IntegerField()
